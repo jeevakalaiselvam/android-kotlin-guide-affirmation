ef27c4056623abad8281e83689a35dd3db509d16
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-05-30 16:33:18 +0530

    Add Affirmation list

--- app/src/main/java/com/gamerguide/android/affirmations/MainActivity.kt
+++ app/src/main/java/com/gamerguide/android/affirmations/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.gamerguide.android.affirmations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.gamerguide.android.affirmations.adapter.ItemAdapter
+import com.gamerguide.android.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDataset = DataSource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
---
+++ app/src/main/java/com/gamerguide/android/affirmations/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.gamerguide.android.affirmations.model
+
+data class Affirmation(val stringResourceId: Int) {
+
+
+}
